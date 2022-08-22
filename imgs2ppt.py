afda6716a0fad22713fbe753c0cce384a2c4ffab
li.zhenye <li.zhenye@qq.com>
2022-08-22 17:53:44 +0800

    支持markdown文件导出为ppt文件

--- imgs2ppt.py
+++ imgs2ppt.py
@@ -19,6 +19,10 @@ functions:
 
 
 from ast import arg, main
+from cmath import log
+from genericpath import isdir, isfile
+from urllib.parse import unquote
+import logging
 import os
 from pptx import Presentation
 from pptx.util import Inches
@@ -36,7 +40,7 @@ def natural_keys(text):
     return [atoi(c) for c in re.split(r'(\d+)', text)]
 
 
-def images2pptx(ppt_img_dir: str, output_file: str) -> Presentation:
+def images2pptx(ppt_img_dir: pathlib.Path, output_file: str) -> Presentation:
     """
     convert images to ppt.
     """
@@ -44,17 +48,36 @@ def images2pptx(ppt_img_dir: str, output_file: str) -> Presentation:
     prs.slide_height = Inches(7.5)
     prs.slide_width = Inches(13.333)
     blank_layout = prs.slide_layouts[6]
-    files = [glob.glob(os.path.join(ppt_img_dir, suffix))
-             for suffix in ["*.png", "*.jpeg", "*.jpg", "*.JPG", "*.PNG", "*.JPEG"]]
-    images = []
-    _ = [images.extend(image_files) for image_files in files]
-    images.sort(key=natural_keys)
+    if isdir(ppt_img_dir):
+        files = [glob.glob(os.path.join(ppt_img_dir, suffix))
+                for suffix in ["*.png", "*.jpeg", "*.jpg", "*.JPG", "*.PNG", "*.JPEG"]]
+        images, comments = [], []
+        _ = [images.extend(image_files) for image_files in files]
+    elif isfile(ppt_img_dir) and (ppt_img_dir.suffix == '.md') or (ppt_img_dir.suffix == '.markdown'):
+        with open(ppt_img_dir, 'r') as f:
+            content = f.read()
+        splited = re.findall(r'!\[(.*?)\]\((.*?)\)\n\n(.*?)\n\n', content)
+        images, comments = [section[1] for section in splited], [section[2] for section in splited]
+        print(images, comments)
+        ppt_file_path = os.path.split(ppt_img_dir)[0]
+        images = [im if isfile(im) else os.path.join(ppt_file_path, im) for im in images]  # 如果是相对路径，则转换为绝对路径
+        images = [im if isfile(im) else unquote(im, 'utf-8') for im in images]  # for markdown file
+    else:
+        logging.error('Input is not a valid dir or file!')
+        return
+    images = [im for im in images if isfile(im)]  # filter out non-existing images
+    images.sort(key=natural_keys)    
     left = top = Inches(0)
     height, width = Inches(7.5), Inches(13.333)
-    for img_path in images:
+    for idx, img_path in enumerate(images):
         slide = prs.slides.add_slide(blank_layout)
+        # add picture to the slide
         slide.shapes.add_picture(
             img_path, left, top, height=height, width=width)
+        # add comment to the slide
+        if len(comments):
+            slide.notes_slide.notes_text_frame.text = comments[idx]
+            print(idx, comments[idx])
     prs.save(output_file)
     return prs
 
@@ -62,7 +85,7 @@ def images2pptx(ppt_img_dir: str, output_file: str) -> Presentation:
 def main():
     parser = argparse.ArgumentParser(description="PPT format change")
     parser.add_argument("-i", '--input_dir',type=pathlib.Path, default='./ppt_imgs',
-                        help="input ppt file")
+                        help="input ppt dir or markdown file")
     parser.add_argument("-o", '--output_file', default="./ppt_output.pptx",
                         type=pathlib.Path, help='output dir or file')
     args = parser.parse_args()
